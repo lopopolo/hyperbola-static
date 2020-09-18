3bdb0578553c1b0ee57f970f3ec3f127a3bd1670
Ryan Lopopolo <rjl@hyperbo.la>
2020-09-17 18:24:48 -0700

    Add commander dispatching to CLI

--- src/cli.js
+++ src/cli.js
@@ -2,12 +2,33 @@
 
 /* eslint-disable no-console */
 
+const { program } = require("commander");
+const { version } = require("../package.json");
 const blogGenerator = require("./generator/blog");
+
+const site = async () => {
+  await blogGenerator();
+};
+
+const blog = async () => {
+  await blogGenerator();
+};
 
 const main = async () => {
   const timer = setInterval(() => {}, 100);
   try {
-    await blogGenerator();
+    program
+      .version(version)
+      .description("Static site generator for hyperbo.la.");
+    program
+      .command("site")
+      .description("Generate the full site and static assets for hyperbo.la/")
+      .action(blog);
+    program
+      .command("blog")
+      .description("Generate blog posts and static assets for hyperbo.la/w/")
+      .action(blog);
+    await program.parseAsync(process.argv);
   } catch (err) {
     console.error("Error: Unhandled exception");
     console.error(err);
